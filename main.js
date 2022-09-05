670541c0754f05c8db4f2bb7b6f94a1134193c71
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-09-05 22:00:14 +0200

    Fix linters errors

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-// ................... Show NavBar & unable Scroll .................
-
+// ................... Show NavBar & unable Scroll .................//
 const menuBtn = document.querySelector('.menu-btn-container');
-let nav = document.querySelector(".menu");
-const noScroll = document.querySelector("body");
+const nav = document.querySelector('.menu');
+const noScroll = document.querySelector('body');
+const navLink = document.querySelectorAll('.nav-link');
 let navShown = false;
-let navLink = document.querySelectorAll(".nav-link");
 
-function closeMenu(){
-    nav.classList.remove("show-menu");
-    noScroll.classList.remove("no-scroll");
-    navShown = false;
-
+function closeMenu() {
+  nav.classList.remove('show-menu');
+  noScroll.classList.remove('no-scroll');
+  navShown = false;
 }
 
-menuBtn.addEventListener("click", () => {
-    if (!navShown) {
-        nav.classList.add("show-menu");
-        noScroll.classList.add("no-scroll");
-        navShown = true;
-    }
-    else {
-       closeMenu();
-    }
-})
+menuBtn.addEventListener('click', () => {
+  if (!navShown) {
+    nav.classList.add('show-menu');
+    noScroll.classList.add('no-scroll');
+    navShown = true;
+  } else {
+    closeMenu();
+  }
+});
 
-navLink.forEach(link => {
-        link.addEventListener("click", () => {
-        closeMenu();
-    })
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
 });
