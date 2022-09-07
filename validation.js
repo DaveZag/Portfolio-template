fcf90525c5b0c78bd650173cf63d4f386fd8422c
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-09-07 19:36:14 +0200

    Add email validation test cases

--- validation.js
+++ validation.js
@@ -1,8 +1,20 @@
 const form = document.querySelector('.form');
 const msg = document.querySelector('.error');
 const email = document.querySelector('#email');
-const emailRegex  = /^([a-z0-9_\-\.]+)@([a-z0-9_\-\.]+)\.([a-z]{2,5})$/
-function showMessage(message){
+const emailRegex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+function showMessage(message) {
+    msg.style.display = 'block';
     msg.innerText = message;
     return msg;
 }
+
+form.addEventListener('submit', (event) => {
+    if(!emailRegex.test(email.value.trim())) {
+        event.preventDefault();
+        showMessage('Please enter a lowercase valid email address');
+    }
+    else{
+        showMessage('');
+    }
+});
