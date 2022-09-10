ba3b44d118506bfe6be8707532e4d5496ef5e905
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-09-10 14:59:36 +0200

    Add markup for the popup window using javascript
    Style the popup window elements

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-/* eslint linebreak-style: ["error", "windows"] */
-
-// ................... Show NavBar & unable Scroll .................//
+// ................... Show NavBar & unable Scroll ................. //
 const menuBtn = document.querySelector('.menu-btn-container');
 const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.menu');
@@ -8,6 +6,7 @@ const noScroll = document.querySelector('body');
 const navLink = document.querySelectorAll('.nav-link');
 let navShown = false;
 
+// function to hide the menu and restore body scroll
 function closeMenu() {
   nav.classList.remove('show-menu');
   btn.classList.remove('menu-active');
@@ -15,6 +14,7 @@ function closeMenu() {
   navShown = false;
 }
 
+// function to show the menu and remove body scroll
 menuBtn.addEventListener('click', () => {
   if (!navShown) {
     nav.classList.add('show-menu');
@@ -26,6 +26,7 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
+// function calling the close menu function each time a menu link is clicked
 navLink.forEach((link) => {
   link.addEventListener('click', () => {
     closeMenu();
@@ -53,6 +54,7 @@ const workGrid = document.createElement('div');// Grid container
 workGrid.classList.add('grid', 'limit-desk');
 workCont.appendChild(workGrid);
 
+// Use object to store Article data
 const articlesData = [
   {
     imgUrl: './assets/images/Img-Placeholder.png',
@@ -64,14 +66,16 @@ const articlesData = [
   },
 
   {
-    title: 'Professional Art Printing Data',
+    title: ['Professional Art Printing Data'],
+    titleDesk: ['Professional Art <br/> Printing Data', 'Data Dashboard Healthcare', 'Website Protfolio', 'Professional Art <br/> Printing Data', 'Data Dashboard Healthcare', 'Website Protfolio'],
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
     technologies: ['html', 'bootstrap', 'Ruby'],
     btnText: 'See Project',
   },
 ];
 
-const firstArticle = document.createElement('article');
+// create first article markup from scratch
+const firstArticle = document.createElement('article'); // article container
 firstArticle.classList.add('main-article', 'flex', 'flex-col', 'flex-center');
 workGrid.appendChild(firstArticle);
 
@@ -118,6 +122,8 @@ articleBlock.appendChild(articleBtn);
 
 // ...........Multiple cards...................
 
+const mediaQuery = matchMedia('(min-width: 768px)');
+
 for (let count = 0; count < 6; count += 1) {
   const workCard = document.createElement('div'); // card container
   workCard.classList.add('work-card', 'flex', 'flex-col', 'end-col-left');
@@ -126,12 +132,19 @@ for (let count = 0; count < 6; count += 1) {
   cardArticle.classList.add('card-article', 'flex', 'flex-col');
   workCard.appendChild(cardArticle);
   const cardBlock = document.createElement('div'); // card text block
-  cardBlock.classList.add('card-block');
+  cardBlock.classList.add('card-block', 'flex', 'flex-col');
   cardArticle.appendChild(cardBlock);
   const cardHeading = document.createElement('header'); // card title
   cardHeading.classList.add('article-title');
-  cardHeading.innerHTML = `<h2>${articlesData[1].title}</h2>`;
-  cardBlock.appendChild(cardHeading);
+
+  // Use condition to display different type of titles depending on the device
+  if (mediaQuery.matches) {
+    cardHeading.innerHTML = `<h2>${articlesData[1].titleDesk[count]}</h2>`;
+    cardBlock.appendChild(cardHeading);
+  } else {
+    cardHeading.innerHTML = `<h2>${articlesData[1].title}</h2>`;
+    cardBlock.appendChild(cardHeading);
+  }
   const cardDesc = document.createElement('p'); // card description
   cardDesc.classList.add('card-art-desc');
   cardDesc.textContent = `${articlesData[1].description}`;
@@ -141,6 +154,7 @@ for (let count = 0; count < 6; count += 1) {
   cardBlock.appendChild(cardTags);
   articlesData[1].technologies.forEach((item) => { // loop to display each tag
     const tag = document.createElement('li');
+    tag.className = 'card-tag';
     const cardLink = document.createElement('a');
     cardLink.href = '#';
     cardLink.innerText = item;
@@ -148,7 +162,90 @@ for (let count = 0; count < 6; count += 1) {
     cardTags.append(tag);
   });
   const cardBtn = document.createElement('button');// card button
-  cardBtn.classList.add('card-btn', 'button');
+  cardBtn.classList.add('card-btn', 'button', 'show-modal');
   cardBtn.textContent = `${articlesData[1].btnText}`;
   workCard.appendChild(cardBtn);
 }
+
+// ................. The Modal ...................
+
+const modalData = {
+  title: ['Multi Post Stories', 'Keeping track of hundreds  of components website'],
+  tags: ['html', 'bootstrap', 'Ruby on rails'],
+  images: ['./assets/images/modal-mobile.svg', './assets/images/modal-desktop.svg'],
+  desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when anunknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  btnClass: ['modal-btn1 button flex', 'modal-btn2 button flex'],
+  btnImg: ['./assets/images/modal-first-btn-icon.svg', './assets/images/modal-second-btn-icon.svg'],
+  btnLink: ['#', '#'],
+  btnText: ['See Live', 'See Source'],
+  btnAlt: ['Live link icon', 'Github icon'],
+  closeBtnData: {
+    class: ['modal-close-btn', 'modal-btn-icon'],
+    imgSrc: './assets/images/modal-close-btn.svg',
+    imgAlt: 'Close popup icon',
+    role: 'button',
+  },
+};
+
+const modal = document.querySelector('.modal');
+
+const modalContainer = document.createElement('div'); // modal container
+modalContainer.classList.add('modal-container', 'flex', 'flex-col');
+modal.appendChild(modalContainer);
+
+const modalHeader = document.createElement('header'); // modal Header
+modalHeader.classList.add('modal-header', 'flex', 'flex-col');
+modalContainer.appendChild(modalHeader);
+
+const modalHeading = document.createElement('h1'); // modal Main Title
+modalHeading.classList.add('modal-title');
+modalHeader.appendChild(modalHeading);
+
+const modalList = document.createElement('ul'); // Modal tag list
+modalList.classList.add('modal-tags', 'flex');
+modalHeader.appendChild(modalList);
+
+// create list items
+for (let count = 0; count <= modalData.tags.length - 1; count += 1) {
+  modalList.innerHTML += `<li class="modal-tag">${modalData.tags[count]}</li>`;
+}
+
+const modalArticle = document.createElement('div'); // modal artcile (Text and picture)
+modalArticle.classList.add('modal-article', 'flex', 'flex-col');
+modalContainer.appendChild(modalArticle);
+
+const modalBlock = document.createElement('div'); // Modal first block
+modalBlock.classList.add('modal-block');
+modalArticle.appendChild(modalBlock);
+
+// display different title and image depending on the device width
+function mediaMatches(media) {
+  if (media.matches) {
+    modalHeading.innerHTML = `${modalData.title[1]}`;
+    modalBlock.innerHTML = ` <img class="modal-img" src="${modalData.images[1]}" alt="Illustration of number ten" />`;
+  } else {
+    modalHeading.innerHTML = `${modalData.title[0]}`;
+    modalBlock.innerHTML = ` <img class="modal-img" src="${modalData.images[0]}" alt="Illustration of number ten" />`;
+  }
+}
+mediaQuery.addEventListener('change', mediaMatches);
+mediaMatches(mediaQuery);
+
+const modalContent = document.createElement('div'); // modal description container
+modalContent.classList.add('modal-content', 'flex', 'flex-col');
+modalContent.innerHTML = `<p class="modal-desc">${modalData.desc}</p>`; // modal description
+modalArticle.appendChild(modalContent);
+
+const btnContainer = document.createElement('div'); // modal buttons container
+btnContainer.classList.add('modal-btns', 'flex');
+
+// Add Links called buttons since they're styled like so
+for (let count = 0; count <= modalData.btnClass.length - 1; count += 1) {
+  btnContainer.innerHTML += `<a href="${modalData.btnLink[count]}" class="${modalData.btnClass[count]}">${modalData.btnText[count]}<img src="${modalData.btnImg[count]}" alt="${modalData.btnAlt[count]}" /></a>`;
+}
+modalContent.appendChild(btnContainer);
+
+const modalCloseBtn = document.createElement('div');
+modalCloseBtn.classList.add('close-btn-container');
+modalCloseBtn.innerHTML = `<span class="${modalData.closeBtnData.class[0]}" role="${modalData.closeBtnData.role}"><img class="${modalData.closeBtnData.class[1]}" src="${modalData.closeBtnData.imgSrc}" alt="${modalData.closeBtnData.imgAlt}"/></span>`;
+modalContainer.appendChild(modalCloseBtn);
